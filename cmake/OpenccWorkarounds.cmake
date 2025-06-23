a07255a99d8a19032b0d5ee13e43967dc9cae663
nopdan <cxprcn@gmail.com>
2025-06-24 07:55:29 +0800

    fix build error

--- cmake/OpenccWorkarounds.cmake
+++ cmake/OpenccWorkarounds.cmake
@@ -4,6 +4,6 @@
 
 # Since OpenCC doesn't include its headers in the binary dir, we need to install
 # them manually.
-file(GLOB LIBOPENCC_HEADERS OpenCC/src/*.hpp
-     "${CMAKE_BINARY_DIR}/OpenCC/src/opencc_config.h")
+file(GLOB LIBOPENCC_HEADERS librime/deps/opencc/src/*.hpp
+     "${CMAKE_BINARY_DIR}/librime/deps/opencc/src/opencc_config.h")
 file(COPY ${LIBOPENCC_HEADERS} DESTINATION "${CMAKE_BINARY_DIR}/include/opencc")
